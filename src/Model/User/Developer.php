f74826e6d34a1965856b65ab41d36eef6624d4ee
Kane <52653784@qq.com>
2018-12-01 15:07:30 +0800

    #

--- src/Model/User/Developer.php
+++ src/Model/User/Developer.php
@@ -9,10 +9,6 @@ class Developer extends Core
 
 	const TYPE = 0;
 
-	protected $hidden = [
-		'type', 'pwd', 'photo', 'group_id', 'status'
-	];
-
 	public function user ($cls)
 	{
 		return $this->hasOne($cls, 'id', 'name');
